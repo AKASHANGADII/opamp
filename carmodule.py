c4fa6a2e49b6516ebbae4a4fcd5590ee69f47dd2
AKASHANGADII <81625153+AKASHANGADII@users.noreply.github.com>
2021-04-08 19:53:16 +0530

    Add files via upload

---
+++ carmodule.py
@@ -0,0 +1,22 @@
+command = ""
+started = input("enter true or false = ").title()
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Already started!")
+        else:
+            print("Started")
+    elif command == "stop":
+        if not started:
+            print("Already stopped!")
+        else:
+            print("stopped")    
+    elif command == "help":
+        print("""
+              start-to start
+              stop- to stop
+            """)
+        break
+else:
+    print("Something is wrong")
